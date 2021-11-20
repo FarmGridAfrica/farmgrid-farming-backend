c07f07b74497eb5def0f479aec3bece3f96c1604
Chuksremi15 <59108605+Chuksremi15@users.noreply.github.com>
2021-11-20 07:17:49 +0100

    lol

--- src/server.js
+++ src/server.js
@@ -35,7 +35,10 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions)); // Use this after the variable declaration
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+}); // Use this after the variable declaration
 
 // Dev logging middleware
 if (process.env.NODE_ENV === "development") {
